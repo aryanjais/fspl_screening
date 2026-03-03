f6182b867a9daea84e74c1e0447e146b297984e3
aryan <aryanj9889@gmail.com>
2026-03-03 19:26:01 +0530

    added skeleton for console accordion

--- src/pages/Scan/index.js
+++ src/pages/Scan/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../Dashboard/components/Header';
 import TopPanel from './components/TopPanel';
+import LiveConsole from './components/LiveConsole';
 
 const ScanDetail = () => {
     return (
@@ -8,6 +9,7 @@ const ScanDetail = () => {
             <Header />
             <div className="dashboard-content">
                 <TopPanel />
+                <LiveConsole />
             </div>
         </>
     )
